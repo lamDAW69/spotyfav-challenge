c6e87dd76a46b58831c92936f233a137636f1983
lamDAW69 <118375353+lamDAW69@users.noreply.github.com>
2024-10-29 13:25:28 +0100

    Connection to db

---
+++ backend/spotifu/src/test/java/com/eoi/spotifu/SpotifuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eoi.spotifu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
