a8c3daa081ca15e4134e03fc4d59102ae9dd9523
Josep <lloretdiazj@gmail.com>
2024-11-15 15:09:20 +0100

    Front updated (home.html)

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,5 @@
 const urlUsers = 'http://localhost:8080/users/1';
 
-// Client ID de tu aplicación Spotify
-// const clientId = '9c134bb930ca48668ffa0b7200979b0f';
-// const redirectUri = 'http://127.0.0.1:5500/frontend/index.html'; // Cambia esto por la URL de tu redirección
-// const scopes = [
-//     'user-library-read', // Permiso para leer las canciones guardadas en la biblioteca del usuario
-//     'playlist-read-private' // Permiso para leer listas de reproducción privadas
-// ];
-
-// const authEndpoint = 'https://accounts.spotify.com/authorize';
 
 const tableSongsDatabase = document.querySelector('#tableSongsDatabase tbody');
 const tableSongsSpotify = document.querySelector('#tableSongsSpotify tbody');
@@ -30,11 +21,11 @@ function anteriorPagina(){
     }
 }
 
-// Función para redirigir al usuario a la autorización de Spotify
-function authorizeSpotify() {
-    const authUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
-    window.location = authUrl;
-}
+// // Función para redirigir al usuario a la autorización de Spotify
+// function authorizeSpotify() {
+//     const authUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+//     window.location = authUrl;
+// }
 
 // Funciones para Usuarios
 async function cargarUsuario() {
@@ -66,72 +57,7 @@ async function cargarUsuario() {
     });
 }
 
-// function guardarToken() {
-//     // Extrae el token del fragmento de la URL (después del '#')
-//     const getTokenFromUrl = () => {
-//         const hash = window.location.hash.substring(1);
-//         const params = new URLSearchParams(hash);
-//         return params.get('access_token');
-//     };
 
-//     const accessToken = getTokenFromUrl();
-
-//     if (accessToken) {
-//         console.log('Token de acceso:', accessToken);
-//         // Guarda el token en localStorage
-//         localStorage.setItem('spotifyAccessToken', accessToken);
-//     }
-// }
-
-// async function obtenerCancionesFavoritas() {
-//     const accessToken = localStorage.getItem('spotifyAccessToken');
-
-//     if (!accessToken) {
-//         console.error('Token de acceso no encontrado');
-//         return;
-//     }
-
-//     try {
-//         const response = await fetch(`https://api.spotify.com/v1/me/tracks?limit=${limit}&offset=${offset}`, {
-//             headers: {
-//                 'Authorization': `Bearer ${accessToken}`
-//             }
-//         });
-
-//         if (response.ok) {
-//             const data = await response.json();
-//             console.log('Canciones favoritas:', data);
-
-//             // Actualiza la tabla con las canciones favoritas de Spotify
-//             tableSongsSpotify.innerHTML = '';
-
-//             data.items.forEach(item => { 
-//                 const song = item.track;
-//                 const row = document.createElement('tr');
-
-//                 const idCell = document.createElement('td'); 
-//                 idCell.textContent = song.id;
-//                 const songNameCell = document.createElement('td');
-//                 songNameCell.textContent = song.name;
-//                 const artistCell = document.createElement('td');
-//                 artistCell.textContent = song.artists.map(artist => artist.name).join(', ');
-//                 const albumCell = document.createElement('td');
-//                 albumCell.textContent = song.album.name;
-
-//                 row.appendChild(idCell);
-//                 row.appendChild(songNameCell);
-//                 row.appendChild(artistCell);
-//                 row.appendChild(albumCell);
-
-//                 tableSongsSpotify.appendChild(row);
-//             });
-//         } else {
-//             console.error('Error al obtener las canciones favoritas:', response.status);
-//         }
-//     } catch (error) {
-//         console.error('Error al conectar con la API de Spotify:', error);
-//     }
-// }
 
 
 async function getSpotifyAccessToken() {
@@ -232,8 +158,8 @@ getSpotifyAccessToken().then(token => {
             const song = track;
             const row = document.createElement('tr');
     
-            const idCell = document.createElement('td'); 
-            idCell.textContent = song.id;
+            // const idCell = document.createElement('td'); 
+            // idCell.textContent = song.id;
             const songNameCell = document.createElement('td');
             songNameCell.textContent = song.name;
             const artistCell = document.createElement('td');
@@ -241,7 +167,7 @@ getSpotifyAccessToken().then(token => {
             const albumCell = document.createElement('td');
             albumCell.textContent = song.album;
     
-            row.appendChild(idCell);
+            // row.appendChild(idCell);
             row.appendChild(songNameCell);
             row.appendChild(artistCell);
             row.appendChild(albumCell);
@@ -259,46 +185,6 @@ getSpotifyAccessToken().then(token => {
 });
 
 
-// function extractTrackIds(data) {
-//     if (!data.tracks || !data.tracks.items) {
-//         console.error("Formato de datos incorrecto o datos incompletos.");
-//         return [];
-//     }
-
-//     // Extraer solo los IDs de las canciones
-//     const trackIds = data.tracks.items.map(track => track.id);
-
-//     console.log("IDs de las canciones extraídas:", trackIds);
-
-//     // Retorna la lista de IDs
-//     return trackIds;
-// }
-
-// // Ejemplo de uso con el JSON proporcionado
-// const jsonResponse = {
-//     "tracks": {
-//         "href": "https://api.spotify.com/v1/search?query=Master&type=track&offset=0&limit=20",
-//         "items": [
-//             {
-//                 "id": "54bm2e3tk8cliUz3VSdCPZ",
-//                 "name": "Master of Puppets (Remastered)"
-//             },
-//             {
-//                 "id": "6kaYDcX9dNmJE5OU71IUby",
-//                 "name": "Masterpiece"
-//             }
-//         ]
-//     }
-// };
-
-// const trackIds = extractTrackIds(jsonResponse);
-
-// // Ejemplo de cómo puedes almacenar estos IDs, por ejemplo, en una base de datos o enviarlos a otra API.
-// console.log(trackIds);
-
-
-// guardarToken();
-// obtenerCancionesFavoritas();
 
 document.getElementById('toggleTheme').addEventListener('click', () => {
     const body = document.body;
